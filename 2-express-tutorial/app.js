d9ef638e497612d6248453330db80ad1a5c5d710
dinhhuy123 <huycholon93@gmail.com>
2022-11-18 20:21:32 +0700

    fifth commit

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const { people } = require('./data');
+
+const people = require('./routes/people');
+const auth = require('./routes/auth');
 
 // static assets
 app.use(express.static('./methods-public'));
@@ -9,25 +11,8 @@ app.use(express.urlencoded({ extended: false }));
 // parse json
 app.use(express.json());
 
-app.get('/api/people', (req, res) => {
-    res.status(200).json({ success: true, people: people });
-});
-
-app.post('/api/people', (req, res) => {
-    const { name } = req.body;
-    if (!name) {
-        return res.status(201).json({ success: false, msg: 'Please provide name value' });
-    }
-    res.status(201).send({ success: true, person: name });
-});
-
-app.post('/login', (req, res) => {
-    const { name } = req.body;
-    if (name) {
-        return res.status(200).send(`Welcome ${name}`);
-    }
-    res.status(401).send('Please Provide Credentials');
-});
+app.use('/api/people', people);
+app.use('/login', auth);
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000....');
